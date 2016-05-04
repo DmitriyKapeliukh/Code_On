1fe1d0f9fa2ebeac92b45ba1b28381b17fbb3bc9
Dmytro Kapeliukh <Dmytro_kapeliuk@epam.com>
2016-05-04 11:48:50 +0300

    commit

--- src/PoolPuzzleOne.java
+++ src/PoolPuzzleOne.java
@@ -2,10 +2,27 @@
  * Created by Dmytro_Kapeliukh on 3/27/2016.
  */
 public class PoolPuzzleOne {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         int x = 0;
-        int y=1;
-        System.out.println("Variable " + y);
+        while (x < 4) {
+                System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+                System.out.print("n");
+            if (x > 1) {
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("noys");
+            }
+            if (x < 1) {
+                System.out.print("oise");
+            }
+                System.out.println("");
+         x = x + 1;
+            }
 
+        }
     }
-}
